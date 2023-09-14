527a780b4dcd162f502b35e19adc7b410e5ff26c
Rumahoy <tryw8tb8@gmail.com>
2023-09-15 00:01:44 +0300

    added googleService functionality to index.js

--- 07_CLI_Uploader/index.js
+++ 07_CLI_Uploader/index.js
@@ -2,9 +2,14 @@ import { readFile } from "node:fs/promises";
 import path from "node:path";
 import axios from "axios";
 import { input, confirm } from "@inquirer/prompts";
+import googleService from "./googleService.js";
+
+const FOLDER_NAME = "CLI Uploader";
 
 const cliUploader = async () => {
   try {
+    await googleService.auth();
+
     const inputData = await input({
       message:
         "Drag and drop your image to terminal and press ENTER for upload",
@@ -24,7 +29,36 @@ const cliUploader = async () => {
       });
     }
 
+    let folderId = await googleService.getFolderId(FOLDER_NAME);
+    if (!folderId) {
+      const newFolder = await confirm({
+        message: `Folder with name ${FOLDER_NAME} not found. Would you like to create it?\n (if you don't want to create it then the image will be uploaded to the root directory of your google drive)`,
+      });
+      if (newFolder) {
+        const createdFolder = await googleService.createFolder(FOLDER_NAME);
+        folderId = createdFolder.id;
+      }
+    }
+
     const image = await readFile(imagePath);
+    const file = await googleService.uploadFile(
+      `${name}${ext}`,
+      image,
+      folderId
+    );
+
+    let fileLink = await googleService.getFileLink(file.id);
+
+    const short = await confirm({
+      message: "Would you like to shorten your link?",
+      default: false,
+    });
+
+    if (short) {
+      fileLink = await getTinyUrl(fileLink);
+    }
+
+    console.log(`Your ${short ? "shorten" : ""} link is: ${fileLink}`);
   } catch (e) {
     throw new Error(e.message);
   }
@@ -50,3 +84,5 @@ async function getTinyUrl(url) {
     throw new Error(e.message);
   }
 }
+
+cliUploader();
