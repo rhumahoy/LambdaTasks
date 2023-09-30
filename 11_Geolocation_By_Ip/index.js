7e29a8b965e5781b481be90dbef06c572c6b50eb
Rumahoy <tryw8tb8@gmail.com>
2023-09-30 19:04:29 +0300

    added sendDataByIp function

--- 11_Geolocation_By_Ip/index.js
+++ 11_Geolocation_By_Ip/index.js
@@ -12,8 +12,28 @@ app.get("/api/ip", (req, res) => {
     req.headers["x-forwarded-for"] ||
     req.socket.remoteAddress;
 
+    sendDataByIp(ip, res);
 });
 
 app.listen(5000, () => {
   console.log("Server is litening on port 5000...");
 });
+
+function sendDataByIp(ip, res) {
+  try {
+    const intIp = ip2Int(ip);
+    if (!intIp) {
+      throw new Error("Invalid input address");
+    }
+
+    const data = findByIntIp(DB, intIp);
+    if (!data) {
+      throw new Error(`Cannot find data for ip: ${ip}`);
+    }
+
+    return res.status(200).json({ ip, ...data });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(404).json({ success: false, message: error.message });
+  }
+}
