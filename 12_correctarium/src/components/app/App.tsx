43f0b9c8772dc4e85c21f9758947e4efb3df2073
Rumahoy <tryw8tb8@gmail.com>
2023-10-12 03:36:40 +0300

    update App, index.css

--- 12_correctarium/src/components/app/App.tsx
+++ 12_correctarium/src/components/app/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import MakeOrder from "../MakeOrder/MakeOrder";
 
 function App() {
+  const handleSubmit = () => {};
   return (
-    <main className="main">
-
+    <main className='main'>
+      <MakeOrder onSubmit={handleSubmit} />
     </main>
   );
 }
